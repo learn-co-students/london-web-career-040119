67f350154b1483f7953a1696725be0a858350288
Wachira <w.ndaiga@gmail.com>
2019-06-10 18:11:03 +0100

    sushi saga review

---
+++ 38-sushi-saga-review/sushi-saga-client/src/components/MoreButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MoreButton = (props) => {
+    return <button onClick={props.shiftSushis}>
+            More sushi!
+          </button>
+}
+
+export default MoreButton
